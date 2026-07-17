92baf2dfbbba71e2e18fafc0ac96fa02fb3d188f
ram7203 <raghuramvasco@gmail.com>
2026-07-17 19:10:31 +0530

    auth service dependencies and docker file

---
+++ AuthService/src/main/java/com/auth/service/model/User.java
@@ -0,0 +1,29 @@
+package com.auth.service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
